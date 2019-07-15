454556ac5f80c9fe8db71f7b769615141d966988
sai236 <569284969@qq.com>
2019-07-15 22:32:48 +0800

    gg

---
+++ src/main/java/jenkinsTest2/test1.java
@@ -0,0 +1,13 @@
+package jenkinsTest2;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class test1 {
+	
+	@Test
+	public void t() {
+		Assert.assertTrue(true);
+	}
+
+}
---
+++ src/main/java/jenkinsTest2/test2.java
@@ -0,0 +1,12 @@
+package jenkinsTest2;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class test2 {
+
+	@Test
+	public void t() {
+		Assert.assertTrue(true);
+	}
+}
